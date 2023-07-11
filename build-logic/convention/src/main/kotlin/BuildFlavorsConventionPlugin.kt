de722b18e1cdc2f93327bb4ae13e0d47e44069bd
Apriega77 <apri.ega@samudrainovasi.co.id>
2023-07-11 19:40:58 +0700

    Setup CICD

--- build-logic/convention/src/main/kotlin/BuildFlavorsConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/BuildFlavorsConventionPlugin.kt
@@ -4,6 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class BuildFlavorsConventionPlugin : Plugin<Project> {
+    //test
     override fun apply(target: Project) {
         with(target) {
             androidAppConfiguration {
@@ -22,6 +23,7 @@ class BuildFlavorsConventionPlugin : Plugin<Project> {
                                 "${AppConfig.ENABLE_SECURITY}",
                             )
                             versionName = AppConfig.generateVersionBuild()
+                            resValue("string", "app_name", "skeleton code-dev")
                         }
                         create("sit") {
                             applicationId = AppConfig.APPLICATION_ID
@@ -32,6 +34,7 @@ class BuildFlavorsConventionPlugin : Plugin<Project> {
                                 "${AppConfig.ENABLE_SECURITY}",
                             )
                             versionName = AppConfig.generateVersionBuild()
+                            resValue("string", "app_name", "skeleton code-sit")
                         }
                         create("uat") {
                             applicationId = AppConfig.APPLICATION_ID
@@ -41,10 +44,12 @@ class BuildFlavorsConventionPlugin : Plugin<Project> {
                                 "ENABLE_SECURITY",
                                 "${AppConfig.ENABLE_SECURITY}",
                             )
+                            resValue("string", "app_name", "skeleton code-uat")
                         }
                         create("prod") {
                             applicationId = AppConfig.APPLICATION_ID
                             buildConfigField("boolean", "ENABLE_SECURITY", "${true}")
+                            resValue("string", "app_name", "skeleton code")
                         }
                     }
                 }
@@ -52,3 +57,5 @@ class BuildFlavorsConventionPlugin : Plugin<Project> {
         }
     }
 }
+
+//123
